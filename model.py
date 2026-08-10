22dea854521373f5ef360343ecfac744b1917f0c
Arda <62362581+atasoyarda@users.noreply.github.com>
2026-08-10 06:32:03 +0300

    Step 57: initialize_w_random

--- model.py
+++ model.py
@@ -473,7 +473,7 @@ def average_nll(p_matrix, data):
 import numpy as np
 
 def initialize_w_random(vocab_size, rng):
-    """Return a (vocab_size, vocab_size) float64 matrix of N(0,1) samples drawn from rng."""
+    """(vocab_size, vocab_size) float64 matrix of N(0,1) samples drawn from rng."""
    
     return rng.standard_normal(size = (vocab_size, vocab_size))
 
