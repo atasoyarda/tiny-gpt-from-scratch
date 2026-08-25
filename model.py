05cc2dc6e2b522b614fcdccf55a8ffa5b12d5118
Arda <62362581+atasoyarda@users.noreply.github.com>
2026-08-25 05:17:14 +0300

    Step 74: linear_forward

--- model.py
+++ model.py
@@ -677,8 +677,12 @@ def sample_from_neural_bigram(w, start_id, num_tokens, itos):
 
     return decode_ids(tokens, itos)
 
-# Step 74 - linear_forward (not yet solved)
-# TODO: implement
+# Step 74 - linear_forward
+def linear_forward(x, w):
+
+    Y = x@w
+    return {'y': Y,
+            'cache': {'x': x, 'w': w}}
 
 # Step 75 - derive_dx_on_paper (not yet solved)
 # TODO: implement
